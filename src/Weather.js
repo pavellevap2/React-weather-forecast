ce380cbc2b933939ae0adb4e5368e9233036d72b
Your Name <youremail@domain.com>
2018-02-10 15:07:02 +0300

    fetch

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 
 let getWeater = () => {
-    fetch("https://api.darksky.net/forecast/adb7b56c5cf973ce38ac44e615433989/37.8267,-122.4233")
-        .then(
-            function(response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-
-                response.json().then(function(data) {
-                    console.log(data);
-                });
-            }
-        )
-        .catch(function(err) {
-            console.log('Fetch Error :-S', err);
+    fetch("https://api.darksky.net/forecast/0123456789abcdef9876543210fedcba/42.3601,-71.0589",{
+        method : "GET",
+        mode : "cors"
+    })
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(text) {
+            console.log('Request successful', text);
+        })
+        .catch(function(error) {
+            console.log('Request failed', error)
         });
 
 }
@@ -25,9 +21,9 @@ class Weather extends React.Component {
     render(){
         return(
             <div>
-            <button onClick={() => getWeater()}>click</button>
+                <button onClick={() => getWeater()}>click</button>
             </div>
-    )
+        )
     }
 }
 export default Weather;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Weather from './Weather';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Weather />, document.getElementById('root'));
-registerServiceWorker();
+
