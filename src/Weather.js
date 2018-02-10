bef121edc02ba4fd386bca430a8a472ecec3648e
Your Name <youremail@domain.com>
2018-02-10 14:21:52 +0300

    rm

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
+
 let getWeater = () => {
-
-    var myInit = { method: 'GET',
-        mode: 'cors',
-        cache: 'default' };
-
-    fetch("https://api.darksky.net/forecast/adb7b56c5cf973ce38ac44e615433989/37.8267,-122.4233",myInit)
+    fetch("https://api.darksky.net/forecast/adb7b56c5cf973ce38ac44e615433989/37.8267,-122.4233")
         .then(
             function(response) {
                 if (response.status !== 200) {
@@ -14,7 +10,6 @@ let getWeater = () => {
                     return;
                 }
 
-                // Examine the text in the response
                 response.json().then(function(data) {
                     console.log(data);
                 });
