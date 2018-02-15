c2f9e59d339cecb23f47ae5df4fb61c3b29254be
Your Name <youremail@domain.com>
2018-02-15 23:31:28 +0300

    changes

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Sidebar.css";
 
-// function initAutocomplete() {
-//     const autocomplete = new window.google.maps.places.Autocomplete(
-//         (document.getElementById("autocomplete")),
-//         {types: ['(cities)']});
-//         autocomplete.addListener("place_changed", function() { let place = autocomplete.getPlace();
-//         console.log("place:", place) })
-// }
+function Input(props) {
+    return (
+        <input type="text" className="Sidebar-input" placeholder="Enter city" id="autocomplete"
+               onChange={props.onChange}
+               onFocus={props.onFocus}
+               value={props.inputValue}/>
+    )
+}
 
 function Sidebar(props) {
     return(
@@ -18,10 +19,11 @@ function Sidebar(props) {
                 <option value="Select format">....</option>
                 {props.makeSelect}
             </select>
-            <input type="text" className="Sidebar-input" placeholder="Enter city" id="autocomplete"
-                   onChange={props.onChange}
-                   onFocus={props.onFocus}
-                   value={props.inputValue}/>
+            <Input
+                onChange={props.onChange}
+                onFocus={props.onFocus}
+                value={props.inputValue}
+            />
             <button className="Sidebar-search_btn" onClick={props.onClick}>Search</button>
         </div>
     )
